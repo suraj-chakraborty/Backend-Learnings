6c464b6611527ba7086ed90cdabc02b3d0f23442
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-05-05 22:01:51 +0000

    connect with mongodb

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,21 +1,38 @@
 const express = require("express");
+const asyncHandler = require("express-async-handler");
 const router = express.Router();
 
 // routes
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "get goals" });
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "get goals" });
+  })
+);
 
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "set goals" });
-});
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+      res.status(400).json({ message: "text field is empty" });
+    } else {
+      res.status(200).json({ message: "set goals" });
+    }
+  })
+);
 
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `update goals ${req.params.id}` });
-});
+router.put(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `update goals ${req.params.id}` });
+  })
+);
 
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `delete goals ${req.params.id}` });
-});
+router.delete(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `delete goals ${req.params.id}` });
+  })
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT;
 
 const app = express();
 
+// middleware
+// --------------------<🥅>----------------
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server is running at port ${port}`));
