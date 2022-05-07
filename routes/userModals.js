522821a253fad496c33cf5df03870b7cb1d0276d
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-05-07 15:46:30 +0000

    user modal

---
+++ routes/userModals.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please add a name"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "please add a email"],
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "please add a passward"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
