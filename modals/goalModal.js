f3336967ea46991d49d79937b48c1c5bfc34991c
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-05-07 15:49:39 +0000

    connecting data with user

--- modals/goalModal.js
+++ modals/goalModal.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 const goalSchema = mongoose.Schema(
   {
+    // connecting data with the user
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     text: {
       type: String,
       required: [true, "Please add a text value"],
